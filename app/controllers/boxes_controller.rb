0957fb97ff756abe481adefece5165e1345c90eb
Fred Chapman <areinei123@gmail.com>
2015-07-29 22:49:40 -0400

    added pagination for boxes index, fixed spacing

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,5 +1,5 @@
 class BoxesController < ActionController::Base
   def index
-    @boxes = Box.all.order(created_at: :desc )
+    @boxes = Box.all.order(created_at: :desc).page(params[:page]).per(5)
   end
 end
--- spec/features/box/box_index_spec.rb
+++ spec/features/box/box_index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'boxes index' do
   let!(:user) {FactoryGirl.create(:user)}
   let!(:box) {FactoryGirl.create(:box)}
+  
   scenario 'user views the bexes index' do
     visit boxes_path
     expect(page).to have_content(box.month)
